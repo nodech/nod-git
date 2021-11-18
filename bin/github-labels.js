db6374abbd370b9fcf250703fbc467dd95451875
Nodari Chkuaselidze <nodar.chkuaselidze@gmail.com>
2021-11-18 18:24:34 +0400

    github-labels: add import.

--- bin/github-labels.js
+++ bin/github-labels.js
@@ -2,10 +2,10 @@
 
 'use strict';
 
-const GH_API = require('../lib/gh-api');
-const assert = require('assert');
-const brq = require('brq');
+const fs = require('fs');
 const path = require('path');
+const brq = require('brq');
+const GH_API = require('../lib/gh-api');
 const {question, NError} = require('../lib/utils');
 const {getConfigs, HOME} = require('../lib/configs');
 
@@ -38,6 +38,7 @@ const action = cfg.str(0, 'help');
     }
     case 'list': {
       check(owner, repo);
+      // Does not list all.... only first 100
       const res = await brq(GH_API.listLabels(owner, repo));
       console.log(JSON.stringify(res.json()));
       break;
@@ -49,11 +50,11 @@ const action = cfg.str(0, 'help');
       if (labels.length === 0)
         throw new NError('Labels not passed.');
 
-      const res = await question(
+      const ans = await question(
         `Deleting labels: \n${labels.join(', ')}\nAre you sure ? (y/n) `
       );
 
-      if (res !== 'y') {
+      if (ans !== 'y') {
         console.log('Aborted.');
         break;
       }
@@ -68,11 +69,11 @@ const action = cfg.str(0, 'help');
       const labelList = await brq(GH_API.listLabels(owner, repo));
       const labelNames = labelList.json().map(l => l.name);
 
-      const res = await question(
+      const ans = await question(
         `Deleting labels: \n${labelNames.join(', ')}\nAre you sure ? (y/n) `
       );
 
-      if (res !== 'y') {
+      if (ans !== 'y') {
         console.log('Aborted.');
         break;
       }
@@ -80,6 +81,54 @@ const action = cfg.str(0, 'help');
       await removeLabels(owner, repo, labelNames);
       break;
     }
+
+    case 'add': {
+      check(owner, repo);
+      const labelFile = cfg.str(1, null);
+
+      if (!labelFile)
+        throw new NError('Could not find file to import labels.');
+
+      let labels;
+      try {
+        labels = JSON.parse(fs.readFileSync(labelFile).toString());
+      } catch (e) {
+        throw new NError(e.message);
+      }
+
+      const names = labels.map(l => l.name);
+      const ans = await question(
+        `Are you sure you want to import:\n${names.join(', ')}\ninto ${owner}/${repo} ? (y/n) `
+      );
+
+      if (ans !== 'y') {
+        console.log('Aborted.');
+        break;
+      }
+
+      for (const label of labels) {
+        const params = {
+          ...auth,
+          ...GH_API.importLabel(owner, repo, label)
+        };
+
+        const res = await brq(params);
+
+        if (!statusCodeOk(res.statusCode)) {
+          const json = res.json();
+          const message = json.message;
+
+          console.log(`Failed to import label "${label.name}" - ${message}`);
+          continue;
+        }
+
+        console.log(`Imported ${label.name}.`);
+      }
+
+      break;
+    }
+    default:
+      help();
   }
 
 })().catch((e) => {
@@ -99,7 +148,8 @@ Actions:
   list  - list project labels (dump)
   delete label-name... - delete project labels
   deleteall - delete all project labels
-  add - ...
+  add labels.json - json file with labels [{ name, color, description }...]
+
 Options:
   -o, --owner=name - owner of the repo
   -r, --repo=repo - repo
@@ -121,10 +171,14 @@ async function removeLabels(owner, repo, labels) {
 
     const res = await brq(params);
 
-    if (res.statusCode === 204) {
+    if (statusCodeOk(res.statusCode)) {
       console.log(`Removed: ${label}.`);
     } else {
       console.log(`Failed to remove "${label}": ${res.json().message}.`);
     }
   }
+}
+
+function statusCodeOk(code) {
+  return code >= 200 && code < 300;
 }
